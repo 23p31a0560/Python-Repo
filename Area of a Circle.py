712492674099a5d06b5c8c87fdef79729866c07d
23p31a0560 <147791928+23p31a0560@users.noreply.github.com>
2024-10-10 13:42:20 +0530

    Uploading code: Area of a Circle.py

---
+++ Area of a Circle.py
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+ public class Area
+    {
+        public static void main(String args[])
+        {
+            Scanner sc=new Scanner(System.in);
+            int r=sc.nextInt();
+            float res=3.14f*r*r;
+            System.out.printf("%.2f",res);
+
+        }
+    }

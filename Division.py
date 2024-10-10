4d5d727a650344465ecdf17652897036d733874d
23p31a0560 <147791928+23p31a0560@users.noreply.github.com>
2024-10-10 13:05:28 +0530

    Uploading code: Division.py

---
+++ Division.py
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class div
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n1,n2;
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+        int res=n1/n2;
+        System.out.println(res);
+        sc.close();
+        
+
+    }
+}

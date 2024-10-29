408ca65ff47b2e1509c874d88348e76d5304d95d
23p31a0560 <147791928+23p31a0560@users.noreply.github.com>
2024-10-29 09:24:47 +0530

    Uploading code: Donation.py

---
+++ Donation.py
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Donate
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        if(y>x)
+        {
+            System.out.println(y-x);
+        }
+   }
+}
